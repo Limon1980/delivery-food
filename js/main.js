d33238ef788ae77f41444bc5ecbac7f960d831b4
Limon1980 <tarkalmob@yandex.ru>
2020-05-06 21:58:07 +1000

    lesson2-16:38min

--- js/main.js
+++ js/main.js
@@ -21,6 +21,7 @@ const logInForm = document.querySelector("#logInForm");
 const loginInput = document.querySelector("#login");
 const userName = document.querySelector(".user-name");
 const buttonOut = document.querySelector(".button-out");
+const cardsRestaurants = document.querySelector(".cards-restaurants");
 // modalAuth.classList.remove('modal-auth'); // удалить класс
 
 let login = localStorage.getItem("gloDelivery"); // получаем логин из localStorage
@@ -106,3 +107,44 @@ function checkAuth() {
 }
 
 checkAuth();
+
+// карточка товара
+
+function createCardRestaurant() {
+  const card = `
+    <a  class="card card-restaurant">
+    <img src="img/tanuki/preview.jpg" alt="image" class="card-image" />
+          <div class="card-text">
+                  <div class="card-heading">
+                    <h3 class="card-title">Тануки</h3>
+                    <span class="card-tag tag">60 мин</span>
+                  </div>
+                <div class="card-info">
+                        <div class="rating">
+                          4.5
+                        </div>
+                    <div class="price">От 1 200 ₽</div>
+                    <div class="category">Суши, роллы</div>
+                </div>
+          </div>
+              </a>
+          `;
+  // вставляем карточку в верстку
+  cardsRestaurants.insertAdjacentHTML("beforeend", card);
+}
+
+createCardRestaurant();
+createCardRestaurant();
+createCardRestaurant();
+
+function openGoods(event) {
+  const target = event.target;
+
+  const restaurant = target.closest(".card-restaurant");
+
+  if (restaurant) {
+    console.log(restaurant);
+  }
+}
+
+cardsRestaurants.addEventListener("click", openGoods);
